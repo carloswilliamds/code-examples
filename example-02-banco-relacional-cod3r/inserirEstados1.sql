d526cbe2ea67efe7b8e35d8e32cb3d370db887b3
Carlos William <carloswilliamds@gmail.com>
2020-01-27 21:26:18 -0200

    start banco de dados

---
+++ example-02-banco-relacional-cod3r/inserirEstados1.sql
@@ -0,0 +1,8 @@
+INSERT INTO estados (nome, sigla, regiao, populacao)
+VALUES ("Acre", "AC", "Norte", 0.83);
+
+INSERT INTO estados (nome, sigla, regiao, populacao)
+VALUES ("São Paulo", "SP", "Sudeste", 12.18),
+       ("Amapá", "AP", "Norte", 0.8),
+       ("Amazonas", "AM", "Norte", 4.06);
+
